21fd5c56eb43a7cddc9db6c718276b841ca64814
Burak ÇINAR <burakcinar07@gmail.com>
2018-04-06 17:35:12 +0300

    Converting to HU Values and Resampling HU Values Completed. 3D Visualization For Slices Added.

--- LungSegmentation.py
+++ LungSegmentation.py
@@ -10,8 +10,10 @@ from sklearn.cluster import KMeans
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import os
 from glob import glob
+import VisualizationHelper as vh
 
 data_path = "C:\\Users\\burak\\Desktop\\New folder\\"
+output_data = "images.npy"
 g = glob(data_path + '/*.dcm')
 
 
@@ -29,4 +31,46 @@ def load_scan(path):
     return slices
 
 
-print(len(load_scan(data_path)))
+def get_pixels_hu(scans):
+    image = np.stack([s.pixel_array for s in scans])
+    image = image.astype(np.int16)
+
+    image[image == -2000] = 0
+
+    intercept = scans[0].RescaleIntercept
+    slope = scans[0].RescaleSlope
+
+    if slope != 1:
+        image = (slope * image.astype(np.float64)).astype(np.int16)
+
+    image += np.int16(intercept)
+
+    return np.array(image, dtype = np.int16)
+
+
+patient_scans = load_scan(data_path)
+imgs = get_pixels_hu(patient_scans)
+
+np.save(output_data, imgs)
+
+imgs_to_process = np.load(output_data)
+
+def resample(image, scan, new_spacing=[1, 1, 1]):
+    # Determine current pixel spacing
+    spacing = map(float, ([scan[0].SliceThickness] + list(scan[0].PixelSpacing)))
+    spacing = np.array(list(spacing))
+
+    resize_factor = spacing / new_spacing
+    new_real_shape = image.shape * resize_factor
+    new_shape = np.round(new_real_shape)
+    real_resize_factor = new_shape / image.shape
+    new_spacing = spacing / real_resize_factor
+
+    image = scipy.ndimage.interpolation.zoom(image, real_resize_factor)
+
+    return image, new_spacing
+
+imgs_after_resampling, spacing = resample(imgs_to_process, patient_scans, [1, 1, 1])
+
+v, f = vh.make_mesh(imgs_after_resampling, 350)
+
---
+++ VisualizationHelper.py
@@ -0,0 +1,75 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import scipy.ndimage
+from skimage import morphology
+from skimage import measure
+from skimage.transform import resize
+from sklearn.cluster import KMeans
+from plotly import __version__
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from plotly.tools import FigureFactory as FF
+from plotly.graph_objs import *
+
+#    Function to show slices
+def sample_stack(stack, rows=2, cols=2, start_with=2, show_every=3):
+    fig,ax = plt.subplots(rows,cols,figsize=[12,12])
+    for i in range(rows*cols):
+        ind = start_with + i*show_every
+        ax[int(i/rows),int(i % rows)].set_title('slice %d' % ind)
+        ax[int(i/rows),int(i % rows)].imshow(stack[ind],cmap='gray')
+        ax[int(i/rows),int(i % rows)].axis('off')
+    plt.show()
+
+
+
+
+
+
+#3D visualization
+def make_mesh(image, threshold=-300, step_size=1):
+    print("Transposing surface")
+    p = image.transpose(2, 1, 0)
+
+    print("Calculating surface")
+    verts, faces, norm, val = measure.marching_cubes(p, threshold, step_size=step_size, allow_degenerate=True)
+    plt_3d(verts, faces)
+    return verts, faces
+
+
+def plotly_3d(verts, faces):
+    x, y, z = zip(*verts)
+
+    print("Drawing")
+
+    # Make the colormap single color since the axes are positional not intensity.
+    #    colormap=['rgb(255,105,180)','rgb(255,255,51)','rgb(0,191,255)']
+    colormap = ['rgb(236, 236, 212)', 'rgb(236, 236, 212)']
+
+    fig = FF.create_trisurf(x=x,
+                            y=y,
+                            z=z,
+                            plot_edges=False,
+                            colormap=colormap,
+                            simplices=faces,
+                            backgroundcolor='rgb(64, 64, 64)',
+                            title="Interactive Visualization")
+    iplot(fig)
+
+
+def plt_3d(verts, faces):
+    print("Drawing")
+    x, y, z = zip(*verts)
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Fancy indexing: `verts[faces]` to generate a collection of triangles
+    mesh = Poly3DCollection(verts[faces], linewidths=0.05, alpha=1)
+    face_color = [1, 1, 0.9]
+    mesh.set_facecolor(face_color)
+    ax.add_collection3d(mesh)
+
+    ax.set_xlim(0, max(x))
+    ax.set_ylim(0, max(y))
+    ax.set_zlim(0, max(z))
+    ax.set_axis_bgcolor((0.7, 0.7, 0.7))
+    plt.show()
