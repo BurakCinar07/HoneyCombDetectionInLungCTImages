a25c00c08d79a8d8a28278a0619051a9d41d04ab
Burak ÇINAR <burakcinar07@gmail.com>
2018-04-28 16:49:06 +0300

    Refactoring. Removed Slow and Low Success Rated Segmentation Functions.
      The method we use give the best result compared to other algorithms.

--- LungSegmentation.py
+++ LungSegmentation.py
@@ -1,15 +1,8 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pydicom
-import cv2
-import pandas as pd
 import scipy.ndimage
-from skimage import measure, morphology, segmentation
-from skimage.transform import resize
-from sklearn.cluster import KMeans
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from skimage import measure, segmentation
 import os
-from glob import glob
 import scipy.ndimage as ndimage
 
 
@@ -47,7 +40,6 @@ def get_pixels_hu(scans):
     return np.array(image, dtype=np.int16)
 
 
-
 #this process is important mention it on your report.
 def resample(image, ref_img, new_spacing=[1, 1, 1]):
     # Determine current pixel spacing
@@ -61,6 +53,7 @@ def resample(image, ref_img, new_spacing=[1, 1, 1]):
     image = scipy.ndimage.interpolation.zoom(image, real_resize_factor, mode='nearest')
     return image, new_spacing
 
+
 def resample_nifti(images, ref_img, new_spacing=[1,1,1]):
     spacing = np.array([ref_img.SliceThickness] + list(ref_img.PixelSpacing), dtype=np.float32)
     resize_factor = spacing / [1, 1, 1]
@@ -69,76 +62,6 @@ def resample_nifti(images, ref_img, new_spacing=[1,1,1]):
     real_resize_factor = new_shape / images.shape
     new_spacing = spacing / real_resize_factor
     return scipy.ndimage.interpolation.zoom(images, real_resize_factor, mode='nearest')
-
-#This method has %17 loss rate.
-def make_lungmask(img, display=False):
-    row_size = img.shape[0]
-    col_size = img.shape[0]
-
-    mean = np.mean(img)
-    std = np.std(img)
-    img = img - mean
-    img = img / std
-
-    middle = img[int(col_size / 5):int(col_size / 5 * 4), int(row_size / 5):int(row_size / 5 * 4)]
-    mean = np.mean(middle)
-    max = np.max(img)
-    min = np.min(img)
-
-    img[img == max] = mean
-    img[img == min] = mean
-
-    kmeans = KMeans(n_clusters=2).fit(np.reshape(middle, [np.prod(middle.shape), 1]))
-    centers = sorted(kmeans.cluster_centers_.flatten())
-    threshold = np.mean(centers)
-    thresh_img = np.where(img < threshold, 1.0, 0.0)
-
-    eroded = morphology.erosion(thresh_img, np.ones([3, 3]))
-    dilation = morphology.dilation(eroded, np.ones([8, 8]))
-
-    labels = measure.label(dilation)
-    label_values = np.unique(labels)
-    regions = measure.regionprops(labels)
-    good_labels = []
-
-    for prop in regions:
-        B = prop.bbox
-        if B[2] - B[0] < row_size / 10 * 9 and B[3] - B[1] < col_size / 10 * 9 and B[0] > row_size / 5 and B[
-            2] < col_size / 5 * 4:
-            good_labels.append(prop.label)
-
-    mask = np.ndarray([row_size, col_size], dtype=np.int8)
-    mask[:] = 0
-
-    for N in good_labels:
-        mask = mask + np.where(labels == N, 1, 0)
-
-    mask = morphology.dilation(mask, np.ones([10, 10]))
-
-    if display:
-        fig, ax = plt.subplots(3, 2, figsize=[12, 12])
-        ax[0, 0].set_title("Original")
-        ax[0, 0].imshow(img, cmap='gray')
-        ax[0, 0].axis('off')
-        ax[0, 1].set_title("Threshold")
-        ax[0, 1].imshow(thresh_img, cmap='gray')
-        ax[0, 1].axis('off')
-        ax[1, 0].set_title("After Erosion and Dilation")
-        ax[1, 0].imshow(dilation, cmap='gray')
-        ax[1, 0].axis('off')
-        ax[1, 1].set_title("Color Labels")
-        ax[1, 1].imshow(labels)
-        ax[1, 1].axis('off')
-        ax[2, 0].set_title("Final Mask")
-        ax[2, 0].imshow(mask, cmap='gray')
-        ax[2, 0].axis('off')
-        ax[2, 1].set_title("Apply Mask on Original")
-        ax[2, 1].imshow(mask * img, cmap='gray')
-        ax[2, 1].axis('off')
-
-        plt.show()
-
-    return mask * img
 
 
 #imgs_after_resampling, spacing = resample(imgs_to_process, patient_scans, [1, 1, 1])
@@ -199,91 +122,6 @@ def normalize(image):
     image[image > 1] = 1.
     image[image < 0] = 0.
     return image
-
-
-# Some of the starting Code is taken from ArnavJain, since it's more readable then my own
-def generate_markers(image):
-    # Creation of the internal Marker
-    marker_internal = image < -400
-    marker_internal = segmentation.clear_border(marker_internal)
-    marker_internal_labels = measure.label(marker_internal)
-    areas = [r.area for r in measure.regionprops(marker_internal_labels)]
-    areas.sort()
-    if len(areas) > 2:
-        for region in measure.regionprops(marker_internal_labels):
-            if region.area < areas[-2]:
-                for coordinates in region.coords:
-                    marker_internal_labels[coordinates[0], coordinates[1]] = 0
-    marker_internal = marker_internal_labels > 0
-    # Creation of the external Marker
-    external_a = ndimage.binary_dilation(marker_internal, iterations=10)
-    external_b = ndimage.binary_dilation(marker_internal, iterations=55)
-    marker_external = external_b ^ external_a
-    # Creation of the Watershed Marker matrix
-    marker_watershed = np.zeros((512, 512), dtype=np.int)
-    marker_watershed += marker_internal * 255
-    marker_watershed += marker_external * 128
-
-    return marker_internal, marker_external, marker_watershed
-
-
-def seperate_lungs(image, Display=False):
-    # Creation of the markers as shown above:
-    marker_internal, marker_external, marker_watershed = generate_markers(image)
-
-    # Creation of the Sobel-Gradient
-    sobel_filtered_dx = ndimage.sobel(image, 1)
-    sobel_filtered_dy = ndimage.sobel(image, 0)
-    sobel_gradient = np.hypot(sobel_filtered_dx, sobel_filtered_dy)
-    sobel_gradient *= 255.0 / np.max(sobel_gradient)
-
-    # Watershed algorithm
-    watershed = morphology.watershed(sobel_gradient, marker_watershed)
-
-    # Reducing the image created by the Watershed algorithm to its outline
-    outline = ndimage.morphological_gradient(watershed, size=(3, 3))
-    outline = outline.astype(bool)
-
-    # Performing Black-Tophat Morphology for reinclusion
-    # Creation of the disk-kernel and increasing its size a bit
-    blackhat_struct = [[0, 0, 1, 1, 1, 0, 0],
-                       [0, 1, 1, 1, 1, 1, 0],
-                       [1, 1, 1, 1, 1, 1, 1],
-                       [1, 1, 1, 1, 1, 1, 1],
-                       [1, 1, 1, 1, 1, 1, 1],
-                       [0, 1, 1, 1, 1, 1, 0],
-                       [0, 0, 1, 1, 1, 0, 0]]
-    blackhat_struct = ndimage.iterate_structure(blackhat_struct, 8)
-    # Perform the Black-Hat
-    outline += ndimage.black_tophat(outline, structure=blackhat_struct)
-
-    # Use the internal marker and the Outline that was just created to generate the lungfilter
-    lungfilter = np.bitwise_or(marker_internal, outline)
-    # Close holes in the lungfilter
-    # fill_holes is not used here, since in some slices the heart would be reincluded by accident
-    lungfilter = ndimage.morphology.binary_closing(lungfilter, structure=np.ones((5, 5)), iterations=3)
-
-    # Apply the lungfilter (note the filtered areas being assigned -2000 HU)
-    segmented = np.where(lungfilter == 1, image, -2000 * np.ones((512, 512)))
-    if Display:
-        print("Sobel Gradient")
-        plt.imshow(sobel_gradient, cmap='gray')
-        plt.show()
-        print("Watershed Image")
-        plt.imshow(watershed, cmap='gray')
-        plt.show()
-        print("Outline after reinclusion")
-        plt.imshow(outline, cmap='gray')
-        plt.show()
-        print("Lungfilter after closing")
-        plt.imshow(lungfilter, cmap='gray')
-        plt.show()
-        print("Segmented Lung")
-        plt.imshow(segmented, cmap='gray')
-        plt.show()
-
-    return segmented
-
 
 """
 masked_lungs = []
